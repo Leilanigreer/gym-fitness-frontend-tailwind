3419690d5c712daa3dbc363f29519aa8d5adb793
Leilanigreer <leilani@lpcgolf.com>
2024-11-16 09:37:43 -0800

    updated LogoutLink to Tailwind

--- src/LogoutLink.jsx
+++ src/LogoutLink.jsx
@@ -1,7 +1,7 @@
 import apiClient from "./config/axios";
 import { Link } from "react-router-dom";
 
-export function LogoutLink() {
+export function LogoutLink({ className }) {
   const handleClick = (event) => {
     event.preventDefault();
     delete apiClient.defaults.headers.common["Authorization"];
@@ -10,8 +10,13 @@ export function LogoutLink() {
   };
 
   return (
-    <Link className="nav-link" to="/" onClick={handleClick}>
-      Logout
+    <Link 
+      className={`relative group ${className}`}
+      to="/" 
+      onClick={handleClick}
+    >
+      <span className="text-white/90 group-hover:text-white transition-colors duration-200">Logout</span>
+      <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white group-hover:w-full transition-all duration-200"></span>
     </Link> 
   );
 }
