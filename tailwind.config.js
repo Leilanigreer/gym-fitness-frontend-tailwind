3dd69757be238f6fb69c3d81470a4e3d256865e0
Leilanigreer <leilani@lpcgolf.com>
2024-12-04 10:34:48 -0800

    fix font issues not loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 // tailwind.config.js
 
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
@@ -8,18 +7,18 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
       colors: {
-        'primary': '#5D3E8D',  // purple
-        'secondary': '#7E5BB7', //  light purple
-        'accent': '#FD9843',    // light orange
-        'orange-background' : '#FFEAD9',
-        'burnt-orange' : "#BF5600",
-        'background': '#F1EDE6', // beige background
+        'primary': '#5D3E8D',
+        'secondary': '#7E5BB7',
+        'accent': '#FD9843',
+        'orange-background': '#FFEAD9',
+        'burnt-orange': "#BF5600",
+        'background': '#F1EDE6',
       }
     },
   },
   plugins: [],
 }
-
-
-
